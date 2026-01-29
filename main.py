4122fc66552d314801433c9e3c734a777f4617bf
Brenno Mainente <bmmainente@gmail.com>
2026-01-29 20:12:23 -0300

    files corrected in .gitignore

--- main.py
+++ main.py
@@ -4,9 +4,9 @@ import os
 
 load_dotenv() 
 
-llm = OpenAI()
+llm = OpenAI(API_KEY_OPENAI)
 
-pergunta = "me fale sobre os hipopotamos"
+pergunta = "me conte sobre a queda do bitcoin no dia de hoje"
 
 resposta = llm.invoke(pergunta)
 
